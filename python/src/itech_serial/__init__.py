ff9ec7d421dde8252329dc5c31b6c318b5e32c5d
Matthew Mets <matt.mets@cibomahto.com>
2024-07-26 21:50:32 +0200

    Export helper classes from itech_serial

--- python/src/itech_serial/__init__.py
+++ python/src/itech_serial/__init__.py
@@ -4,4 +4,4 @@ __version__ = "0.1.3"
 
 from .it6800 import IT6800
 from .it8500 import IT8500
-from .itech_serial import _InstrumentInterface
+from .itech_serial import _InstrumentInterface, InstrumentException, find_usb
